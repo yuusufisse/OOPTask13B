4f1786e11b02d085b0a9943989c93e6cfde389fd
Yusuf Farah <cyufa001@edu.xamk.fi>
2021-04-22 18:25:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace OOPTask13B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=Thursday;Integrated Security=true;";
+            using (SqlConnection sqlConnection= new SqlConnection(connectionString))
+            {
+                try
+                {
+                    sqlConnection.Open();
+                    Console.WriteLine("State: {0}", sqlConnection.State);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
