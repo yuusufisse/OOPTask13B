340731a858d112564966031a97241dcffd55d45e
Yusuf Farah <cyufa001@edu.xamk.fi>
2021-04-22 20:25:56 +0300

    completed.

--- Program.cs
+++ Program.cs
@@ -13,33 +13,8 @@ namespace OOPTask13B
                 try
                 {
                     sqlConnection.Open();
-                    //Do the work here!
-                    string sql = "INSERT INTO Dentist (Name, TelNum) VALUES (@Name, @TelNum)";
-                    SqlCommand sqlCommand1 = new SqlCommand(sql, sqlConnection);
-                    SqlParameter sqlParameter = new SqlParameter
-                    {
-                        ParameterName="@Name",
-                        Value="Donuld Duck",
-                        SqlDbType=System.Data.SqlDbType.NVarChar
-                    };
-                    sqlCommand1.Parameters.Add(sqlParameter);
-
-                    sqlParameter = new SqlParameter
-                    {
-                        ParameterName="@TelNum",
-                        Value="040678123",
-                        SqlDbType = System.Data.SqlDbType.NVarChar
-                    };
-                    sqlCommand1.Parameters.Add(sqlParameter);
-
-                    int addedOneRow = sqlCommand1.ExecuteNonQuery();
-                    if (addedOneRow == 1)
-                        Console.WriteLine("New staff member added.");
-                    else
-                        Console.WriteLine("For some reason nothing happened.");
-
-                    //Did it go?
-                    string queryString = "SELECT * FROM Dentist";
+                    
+                    string queryString = "SELECT * FROM Dentist WHERE Name LIKE 'g%'";
                     SqlCommand sqlCommand2 = new SqlCommand(queryString, sqlConnection);
                     using(SqlDataReader sqlDataReader=sqlCommand2.ExecuteReader())
                     {
---
+++ dbo.Dentist.data.sql
@@ -0,0 +1,6 @@
+﻿SET IDENTITY_INSERT [dbo].[Dentist] ON
+INSERT INTO [dbo].[Dentist] ([Id], [Name], [TelNum]) VALUES (4, N'Jukka Ikonen', N'040 7890123')
+INSERT INTO [dbo].[Dentist] ([Id], [Name], [TelNum]) VALUES (5, N'Paula Oksanen', N'040 6784321')
+INSERT INTO [dbo].[Dentist] ([Id], [Name], [TelNum]) VALUES (6, N'Sari Kaukorinne', N'050 895479')
+INSERT INTO [dbo].[Dentist] ([Id], [Name], [TelNum]) VALUES (7, N'Gyro Gearloose', N'040 6778321')
+SET IDENTITY_INSERT [dbo].[Dentist] OFF
