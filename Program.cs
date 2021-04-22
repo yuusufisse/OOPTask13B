f1276cc11bc1323f63aa696ddd82e3d1cd8214d0
Yusuf Farah <cyufa001@edu.xamk.fi>
2021-04-22 19:53:54 +0300

    completed.

--- Program.cs
+++ Program.cs
@@ -13,15 +13,41 @@ namespace OOPTask13B
                 try
                 {
                     sqlConnection.Open();
+                    //Do the work here!
+                    string sql = "INSERT INTO Dentist (Name, TelNum) VALUES (@Name, @TelNum)";
+                    SqlCommand sqlCommand1 = new SqlCommand(sql, sqlConnection);
+                    SqlParameter sqlParameter = new SqlParameter
+                    {
+                        ParameterName="@Name",
+                        Value="Donuld Duck",
+                        SqlDbType=System.Data.SqlDbType.NVarChar
+                    };
+                    sqlCommand1.Parameters.Add(sqlParameter);
+
+                    sqlParameter = new SqlParameter
+                    {
+                        ParameterName="@TelNum",
+                        Value="040678123",
+                        SqlDbType = System.Data.SqlDbType.NVarChar
+                    };
+                    sqlCommand1.Parameters.Add(sqlParameter);
+
+                    int addedOneRow = sqlCommand1.ExecuteNonQuery();
+                    if (addedOneRow == 1)
+                        Console.WriteLine("New staff member added.");
+                    else
+                        Console.WriteLine("For some reason nothing happened.");
+
+                    //Did it go?
                     string queryString = "SELECT * FROM Dentist";
-                    SqlCommand sqlCommand = new SqlCommand(queryString, sqlConnection);
-                    using(SqlDataReader sqlDataReader=sqlCommand.ExecuteReader())
+                    SqlCommand sqlCommand2 = new SqlCommand(queryString, sqlConnection);
+                    using(SqlDataReader sqlDataReader=sqlCommand2.ExecuteReader())
                     {
                         if(sqlDataReader.HasRows)
                         {
                             while (sqlDataReader.Read())
                             {
-                                //Save it to a generic collection??
+                                
                                 Console.WriteLine(String.Format("{0}, {1}, {2}", 
                                     sqlDataReader[0], sqlDataReader[1], sqlDataReader[2]));
                             }
